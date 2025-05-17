cb9e058b45a3b3e63b9646496da7d0db1d173ce1
Rohit Singh <rohitrajput95547@gmail.com>
2025-05-17 08:10:32 +0530

    Favor Composition over Inheritance Problem!!!

--- app/src/main/java/com/example/solidprinciples/excode/file2/file2.kt
+++ app/src/main/java/com/example/solidprinciples/excode/file2/file2.kt
@@ -1,2 +1,30 @@
 package com.example.solidprinciples.excode.file2
 
+fun main(){
+
+    val w = Warrior()
+    w.move()
+    w.attack()
+
+    val m = Mage()
+    m.move()
+    m.castSpell()
+}
+
+
+// Problem: Rigid class hierarchy using inheritance
+open class CCharacter {
+    open fun move() = println("Character moves")
+}
+
+class Warrior : CCharacter() {
+    fun attack() = println("Warrior attacks with sword")
+}
+
+class Mage : CCharacter() {
+    fun castSpell() = println("Mage casts fireball")
+}
+
+// Limitations:
+// 1. Can't easily make a Spell-casting Warrior
+// 2. Changing Character affects all subclasses
