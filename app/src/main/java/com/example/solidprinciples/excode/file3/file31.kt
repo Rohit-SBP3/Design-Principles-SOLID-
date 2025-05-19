d872d8813796442996bfe694b23e9ef896931a16
Rohit Singh <rohitrajput95547@gmail.com>
2025-05-19 07:25:31 +0530

    SOLID Principles with Solution examples

---
+++ app/src/main/java/com/example/solidprinciples/excode/file3/file31.kt
@@ -0,0 +1,85 @@
+package com.example.solidprinciples.excode.file3
+
+// Single Responsibility Principle's ✅ Solution
+class UserRepository {
+    fun saveToDb(user: User) { /* Only DB logic */ }
+}
+
+class EmailService {
+    fun sendEmail(user: User) { /* Only email logic */ }
+}
+
+class UserValidator {
+    fun validate(user: User) { /* Only validation */ }
+}
+
+
+// Open/Close Principle's ✅ Solution
+interface PaymentMethod {
+    fun process()
+}
+
+class CreditCard : PaymentMethod {
+    override fun process() { /* Credit logic */ }
+}
+
+class PayPal : PaymentMethod {
+    override fun process() { /* PayPal logic */ }
+}
+
+class PaymentProcessor {
+    fun processPayment(method: PaymentMethod) {
+        method.process()  // No changes needed for new methods!
+    }
+}
+
+// Liskov Substitution Principle's ✅ Solution
+open class MBird
+
+open class FlyingBird : MBird() {
+    fun fly() = println("Flying")
+}
+
+class Penguin : MBird()  // No fly() method, so no violation
+
+
+// Interface Segregation Principle's ✅ Solution
+interface Workable { fun work() }
+interface Eatable { fun eat() }
+interface Sleepable { fun sleep() }
+
+class Human : Workable, Eatable, Sleepable { /* Implements all */
+    override fun work() {
+        TODO("Not yet implemented")
+    }
+
+    override fun eat() {
+        TODO("Not yet implemented")
+    }
+
+    override fun sleep() {
+        TODO("Not yet implemented")
+    }
+}
+class Robot : Workable { /* Only implements work() */
+    override fun work() {
+        TODO("Not yet implemented")
+    }
+}
+
+// Dependency Inversion Principle's ✅ Solution
+interface Switchable {
+    fun turnOn()
+}
+
+class LightBulb : Switchable {
+    override fun turnOn() = println("Bulb on")
+}
+
+class Switch(private val device: Switchable) {  // Depends on abstraction
+    fun operate() = device.turnOn()
+}
+
+// Usage:
+val bulb = LightBulb()
+val switch = Switch(bulb)  // Can switch ANY Switchable device
